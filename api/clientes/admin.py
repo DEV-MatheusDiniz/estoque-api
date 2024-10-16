1303f0738bafbd986e3a1335f1d0cdf833c659a6
Diniz <mamakerrykinh@gmail.com>
2024-10-15 23:27:30 -0300

    add models vendas

--- api/clientes/admin.py
+++ api/clientes/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 from .models.clientes import ClienteModel
 
-# Register your models here.
+
 admin.site.register(ClienteModel)
---
+++ api/vendas/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models.vendas import VendasModel
+
+
+admin.site.register(VendasModel)
